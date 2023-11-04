07ae7a7c36102397c1be5be36d2130c3f715f033
Lucas <thebiglikboss777@gmail.com>
2023-11-04 00:23:13 -0500

    menu hamburguesa listo, hay que cambiar el color

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-let iniciaSesion = document.querySelector("#logIn")
-let form = document.querySelector("#form")
-let principalContainer = document.querySelector("#principal-container")
+let iniciaSesion = document.querySelector("#logIn");
+let form = document.querySelector("#form");
+let principalContainer = document.querySelector("#principal-container");
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const quitar = document.querySelector("#btn-home");
+const btnNew = document.querySelector("#btn-new");
+const products = document.querySelector("#btn-products");
 
 iniciaSesion.addEventListener("click", () => {
     form.style.display = "block";
     principalContainer.className = "difuminado"
 });
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+quitar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+btnNew.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+products.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
